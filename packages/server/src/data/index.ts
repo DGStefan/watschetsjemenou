3da8c6c71158ffcf4dd71a6aca751a9e35846e17
Stefan <stefan@codepress.nl>
2026-07-02 15:54:15 +0200

    Moved data and prepare for more languages

--- packages/server/src/data/index.ts
+++ packages/server/src/data/index.ts
@@ -1,15 +1,15 @@
 import type { WordSets } from "./types";
-import nlEenvoudig from "./nl/eenvoudig";
-import nlGeavanceerd from "./nl/geavanceerd";
+import nlEasy from "./nl/easy";
+import nlAdvanced from "./nl/advanced";
 
 /**
- * Alle woordenlijsten, geordend per taal en niveau. Nu alleen Nederlands.
- * Een taal toevoegen? Maak data/<taal>/<niveau>/ met categoriebestanden en
- * voeg hier een regel toe.
+ * Alle woordenlijsten, geordend per taal en niveau. De mapstructuur is Engels
+ * (data/<taal>/<niveau>/<categorie>.json) zodat extra talen later logisch
+ * passen; de niveau-sleutels blijven de Difficulty-waarden die het spel kent.
  */
 export const wordSets: WordSets = {
   nl: {
-    eenvoudig: nlEenvoudig,
-    geavanceerd: nlGeavanceerd,
+    eenvoudig: nlEasy,
+    geavanceerd: nlAdvanced,
   },
 };
---
+++ packages/server/src/data/nl/advanced/index.ts
@@ -0,0 +1,38 @@
+import { tag } from "../../tag";
+import type { WordEntry } from "../../types";
+import animals from "./animals.json";
+import body from "./body.json";
+import foodDrink from "./food-drink.json";
+import nature from "./nature.json";
+import clothing from "./clothing.json";
+import vehicles from "./vehicles.json";
+import sportsGamesMusic from "./sports-games-music.json";
+import household from "./household.json";
+import buildingsPlaces from "./buildings-places.json";
+import fantasyHolidays from "./fantasy-holidays.json";
+import characters from "./characters.json";
+import abstract from "./abstract.json";
+import sayings from "./sayings.json";
+
+/**
+ * Alle advanced woorden voor het Nederlands, samengevoegd uit de
+ * categoriebestanden in deze map. Nieuwe categorie? Voeg een JSON-bestand toe
+ * en zet er hier een import- + spread-regel bij.
+ */
+const words: WordEntry[] = [
+  ...tag("nl", "animals", animals),
+  ...tag("nl", "body", body),
+  ...tag("nl", "food-drink", foodDrink),
+  ...tag("nl", "nature", nature),
+  ...tag("nl", "clothing", clothing),
+  ...tag("nl", "vehicles", vehicles),
+  ...tag("nl", "sports-games-music", sportsGamesMusic),
+  ...tag("nl", "household", household),
+  ...tag("nl", "buildings-places", buildingsPlaces),
+  ...tag("nl", "fantasy-holidays", fantasyHolidays),
+  ...tag("nl", "characters", characters),
+  ...tag("nl", "abstract", abstract),
+  ...tag("nl", "sayings", sayings),
+];
+
+export default words;
